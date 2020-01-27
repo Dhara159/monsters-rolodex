a601ceebbc9a2bfbb7e7f881eb2aa38995786224
Dhara Vihol <viholdhara@gmail.com>
2020-01-27 22:49:44 +0530

    Updates

---
+++ precache-manifest.819050ef30c3412ff644c63b4dcb43c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c97ee1531bc40adf48588b5f9227c09",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12c6607154ea8ab1a7e5",
+    "url": "/monsters-rolodex/static/css/main.899fdf65.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c6607154ea8ab1a7e5",
+    "url": "/monsters-rolodex/static/js/main.d110d2e2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
